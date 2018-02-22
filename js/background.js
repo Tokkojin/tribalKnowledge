0a191f311dbf7180f531bd858d2a7fadf3253b65
Hayden Le <tokkojin@users.noreply.github.com>
2018-02-22 15:12:16 -0500

    updated icons and created pop-out form from contextmenu click

--- js/background.js
+++ js/background.js
@@ -4,6 +4,20 @@ chrome.runtime.onInstalled.addListener(function() {
   var title = "Annotate \"%s\"";
   var id = chrome.contextMenus.create({"title": title, "contexts":[context],
                                          "id": "context" + context});  
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'github.com', schemes: ['https'] },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+
 });
 
 // add click event
@@ -11,7 +25,14 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 // The onClicked callback function.
 function onClickHandler(info, tab) {
-  var sText = info.selectionText;
-  var url = "https://www.google.com/search?q=" + encodeURIComponent(sText);  
-  window.open(url, '_blank');
+  var formUrl = "../html/popup.html";
+  var highlight = info.selectionText;
+  var w = (screen.width/4);
+  var h = (screen.width/3);
+  var left = (screen.width/2)-(w/2);
+  var top = (screen.height/2)-(h/2);   
+  chrome.windows.create({'url': formUrl, 'type': 'popup', 
+  	'width': w, 'height': h, 'left': left, 'top': top} , function(window) {
+  		$('highlight').val(info.selectionText);
+    });
 };
